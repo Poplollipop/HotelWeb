e2828b6cb3ee981ec0cfda0d70b94e8d7eeb0689
Joker <aaa111334@gmail.com>
2025-03-31 00:57:04 +0800

    have to debug dashboard page

--- src/app/pages/admin/pages/dashboard/dashboard.component.ts
+++ src/app/pages/admin/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { AdminService } from '../../../../service/admin-service/admin.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { RouterLink } from '@angular/router';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    NzButtonModule,
+    NzCardModule,
+    NzSkeletonModule,
+    RouterLink,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
@@ -12,6 +21,7 @@ export class DashboardComponent {
 
   current_page = 1;
   rooms = [];
+  total: any;
 
   constructor(
     private message: NzMessageService,
@@ -24,6 +34,7 @@ export class DashboardComponent {
     this.adminService.getRoom(this.current_page - 1).subscribe(res => {
       console.log(res);
       this.rooms = res;
+      this.total = res.pages * 1;
     })
   }
 
